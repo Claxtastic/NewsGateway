d2078e7766d96fe03f04fba17b05cabdf982917d
claxtont <claxtons4@gmail.com>
2020-05-19 16:02:44 -0500

    Added jacoco and temporarily reverted to JUnit 4

--- app/src/test/java/com/claxtastic/newsgateway/ExampleUnitTest.java
+++ app/src/test/java/com/claxtastic/newsgateway/ExampleUnitTest.java
@@ -1,7 +1,6 @@
 package com.claxtastic.newsgateway;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,6 +10,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+        Article a = new Article("", "", "","","","");
     }
 }
